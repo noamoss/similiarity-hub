895dc9257e2c83767fa4a1a03ecc6506a7ee050b
newone <noamoss@gmail.com>
2021-01-19 22:44:32 +0200

    more linting

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate 
 from models import db, Entity
 
+# App object
+app = Flask(__name__)        
 
-app = Flask(__name__)                   # App object
-
-if app.config["ENV"] == "production":   # check relevant enviroment and load settings
+# check relevant enviroment and load settings
+if app.config["ENV"] == "production":   
     app.config.from_object("config.ProductionConfig")
     print("Production Env")
 else:
     app.config.from_object("config.DevelopmentConfig")
     print("Development Env")
 
-db.init_app(app)                      # initiate connection with the database
-migrate = Migrate(app, db)            # migrate data, if needed
+# initiate connection with the database
+db.init_app(app)                      
+# migrate data, if needed
+migrate = Migrate(app, db)            
 
 
 @app.route('/')
 def hello_world():
     return "Hello, World!"
 
-    
+
 if __name__ == '__main__':            # run the server
     app.run(debug=True)
--- models.py
+++ models.py
@@ -2,9 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class Entity(db.Model):
     __tablename__ = 'entities_types'
-    id = db.Column(db.Integer(), primary_key = True)
+    id = db.Column(db.Integer(),primary_key=True)
     name = db.Column(db.String())
     source = db.Column(db.String())
     features_periodic = db.Column(db.String())
@@ -13,7 +14,8 @@ class Entity(db.Model):
     formula_realtime = db.Column(db.String())
     result_count = db.Column(db.Integer())
 
-    def __init__(self,id, name, source, features_periodic, formula_periodic, features_realtime, formula_realtime, result_count):
+    def __init__(self, id, name, source, features_periodic, formula_periodic,
+                 features_realtime, formula_realtime, result_count):
         self.id = id
         self.name = name
         self.source = source 
@@ -26,8 +28,10 @@ class Entity(db.Model):
     def __reprt__(self):
         return f"""{self.id}. {self.name}:
          source: {self.source}
-         features_periodic: {self.features_periodic} formula_period: {self.formula_periodic}
-         features_realtime: {self.features_realtime} formula_realtime: {self.formula_realtime}        
+         features_periodic: {self.features_periodic} \
+             formula_period: {self.formula_periodic}
+         features_realtime: {self.features_realtime} \
+             formula_realtime: {self.formula_realtime}        
          result_count: {self.result_count}
         """
         
