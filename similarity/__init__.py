ec5627e127e85897afcddb8d6ee1d620f53cc5b9
Or David <ordavidil@gmail.com>
2021-01-30 23:47:54 +0000

    Try to imporove the tests.

--- similarity/__init__.py
+++ similarity/__init__.py
@@ -3,26 +3,31 @@ from flask_migrate import Migrate
 from similarity.models import db
 
 
-# App object
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+
+    # Check relevant environment and load settings.
+    if app.config['ENV'] == 'production':
+        app.config.from_objbect('config.ProductionConfig')
+        print('Production Env')
+    else:
+        app.config.from_object('config.DevelopmentConfig')
+        print('Development Env')
+
+    # Initiate connection with the database.
+    db.init_app(app)
+
+    # Migrate data  (if needed).
+    migrate = Migrate(app, db)
+
+    return app
+
+app = create_app()
 
 # Importing after the `app` definition to avoid circular dependency.
 # Ignoring "flake8" errors about it.
 import similarity.views # noqa: E402,F401,E261
 
-# check relevant environment and load settings
-if app.config['ENV'] == 'production':
-    app.config.from_object('config.ProductionConfig')
-    print('Production Env')
-else:
-    app.config.from_object('config.DevelopmentConfig')
-    print('Development Env')
-
-
-# initiate connection with the database
-db.init_app(app)
-# migrate data, if needed
-migrate = Migrate(app, db)
 
 # run the server
 if __name__ == '__main__':
