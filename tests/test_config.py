ce22afc71f8946552a21517151b8e9471fd4af21
Or David <ordavidil@gmail.com>
2021-02-22 00:24:19 +0000

    Use single quote.

--- tests/test_config.py
+++ tests/test_config.py
@@ -11,29 +11,29 @@ from similarity.config import (
 
 
 @pytest.mark.skipif(
-    "TRAVIS" in os.environ and os.environ["TRAVIS"] == "True",
-    reason="Skipping this test on Travis CI.",
+    'TRAVIS' in os.environ and os.environ['TRAVIS'] == 'True',
+    reason='Skipping this test on Travis CI.',
 )
 def test_development_config(app): # noqa F811
     app = app(DevelopmentConfig)
-    DB_URL = get_env_db_url("development")
-    assert app.config["DEBUG"]
-    assert not app.config["TESTING"]
-    assert app.config["SQLALCHEMY_DATABASE_URI"] == DB_URL
+    DB_URL = get_env_db_url('development')
+    assert app.config['DEBUG']
+    assert not app.config['TESTING']
+    assert app.config['SQLALCHEMY_DATABASE_URI'] == DB_URL
 
 
 def test_testing_config(app): # noqa F811
     app = app(TestingConfig)
-    DB_URL = get_env_db_url("testing")
-    assert app.config["DEBUG"]
-    assert app.config["TESTING"]
-    assert not app.config["PRESERVE_CONTEXT_ON_EXCEPTION"]
-    assert app.config["SQLALCHEMY_DATABASE_URI"] == DB_URL
+    DB_URL = get_env_db_url('testing')
+    assert app.config['DEBUG']
+    assert app.config['TESTING']
+    assert not app.config['PRESERVE_CONTEXT_ON_EXCEPTION']
+    assert app.config['SQLALCHEMY_DATABASE_URI'] == DB_URL
 
 
 def test_production_config(app): # noqa F811
     app = app(ProductionConfig)
-    DB_URL = get_env_db_url("production")
-    assert not app.config["DEBUG"]
-    assert not app.config["TESTING"]
-    assert app.config["SQLALCHEMY_DATABASE_URI"] == DB_URL
+    DB_URL = get_env_db_url('production')
+    assert not app.config['DEBUG']
+    assert not app.config['TESTING']
+    assert app.config['SQLALCHEMY_DATABASE_URI'] == DB_URL
